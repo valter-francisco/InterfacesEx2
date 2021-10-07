c9f01ac7902da39dd718a8ba424c00d79c9665d0
Valter Francisco <valter_chico@hotmail.com>
2021-10-07 15:03:55 +0100

    Add project files.

---
+++ InterfacesEx2/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesEx2
+{
+    class Animal : IAnimal
+    {
+        public string species { get; set; }
+
+        public string talk { get; set; }
+
+        public void Talk()
+        {
+            Console.WriteLine("The {0} goes {1}!", this.species, this.talk);
+        }
+    }
+}
---
+++ InterfacesEx2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace InterfacesEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Dog = new Animal();
+            Dog.species = "dog";
+            Dog.talk = "bark";
+
+            Animal Cat =  new Animal();
+            Cat.species = "cat";
+            Cat.talk = "meow";
+
+            Animal Cow = new Animal();
+            Cow.species = "cow";
+            Cow.talk = "moo";
+
+            Animal Sheep = new Animal();
+            Sheep.species = "sheep";
+            Sheep.talk = "bah";
+
+            Animal Duck = new Animal();
+            Duck.species = "duck";
+            Duck.talk = "quack";
+
+
+            while (true)
+            {
+                Console.WriteLine("Choose an animal (1 to 5) and the application will display it's voice.");
+                Console.WriteLine("1 - Dog \n2 - Cat \n3 - Cow \n4 - Sheep \n5 - Duck \n0 - Quit");
+
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Dog.Talk();
+                        Console.WriteLine("Press any key to continue. . .");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "2":
+                        Cat.Talk();
+                        Console.WriteLine("Press any key to continue. . .");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "3":
+                        Cow.Talk();
+                        Console.WriteLine("Press any key to continue. . .");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "4":
+                        Sheep.Talk();
+                        Console.WriteLine("Press any key to continue. . .");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "5":
+                        Duck.Talk();
+                        Console.WriteLine("Press any key to continue. . .");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please insert a valid input. . .");
+                        break;
+                }
+            }
+        }
+    }
+}
